b278f3080348458d81e175997f022d59d6f8d9de
Kevlmt <97613786+Kevlmt@users.noreply.github.com>
2022-08-02 19:09:55 +0200

    quest 2 ok

---
+++ src/app/user-profil/user-profil.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profil',
+  templateUrl: './user-profil.component.html',
+  styleUrls: ['./user-profil.component.scss'],
+})
+export class UserProfilComponent implements OnInit {
+  User = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+  };
+  isHidden: boolean = true;
+
+  ngOnInit(): void {}
+
+  handleHide() {
+    this.isHidden = !this.isHidden;
+  }
+}
