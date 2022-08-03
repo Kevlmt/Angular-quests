14520ce763ebdf7206a3d8ee73220f5c7d9ca4d7
Kevlmt <97613786+Kevlmt@users.noreply.github.com>
2022-08-03 15:02:11 +0200

    quest 4 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ListMoviesComponent } from './list-movies/list-movies.component';
-import { HandleShowMovieDirective } from './handle-show-movie.directive';
-import { HandleChangeStyleDirective } from './handle-change-style.directive';
+import { MenuComponent } from './menu/menu.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent, ListMoviesComponent, HandleShowMovieDirective, HandleChangeStyleDirective],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    UserProfileComponent,
+    SignUpComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
